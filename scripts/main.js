14bf060b43908629dbd0942db676b8d1fc34168c
Sergei Bobrov <sergey.b@digital-spectr.ru>
2022-08-01 23:49:52 +0800

    wedding init: part 1

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,260 @@
+// GUEST LIST
+let guest_list = {
+  groom_parents: {
+    persons: [
+    {name: "Папа", description: "", photo: null},
+    {name: "Мама", description: "", photo: null},
+  ],
+  title: "Same title"
+},
+  bride_parents: {
+    persons: [
+    {name: "Мама", description: "", photo: null},
+  ],
+  title: "Same title"
+},
+  groom_sister_family: {
+    persons: [
+    {name: "Оля", description: "", photo: null},
+    {name: "Олег", description: "", photo: null},
+    {name: "Юра", description: "", photo: null},
+  ],
+  title: "Same title"
+},
+  aunt_eugenia: {
+    persons: [
+    {name: "Женя", description: "", photo: null},
+    {name: "Максим", description: "", photo: null},
+    {name: "Юля", description: "", photo: null},
+    {name: "Саша", description: "", photo: null},
+  ],
+  title: "Same title"
+},
+  aunt_anna: {
+    persons: [
+    {name: "Аня", description: "", photo: null},
+    {name: "Саша", description: "", photo: null},
+  ],
+  title: "Same title"
+},
+  aunt_tatyana: {
+    persons: [
+    {name: "Татьяна", description: "", photo: null}
+  ],
+  title: "Same title"
+},
+  dasha: {
+    title: "Даша",
+    persons: [
+      {name: "Даша", description: "", photo: null}
+    ]
+  },
+  cousin_natasha: {
+    persons: [
+    {name: "Наташа", description: "", photo: null},
+    {name: "Саша", description: "", photo: null},
+    {name: "Дима", description: "", photo: null},
+    {name: "Артём", description: "", photo: null},
+  ],
+  title: "Same title"
+},
+  cousin_eugen: {
+    persons: [
+    {name: "Женя", description: "", photo: null},
+    {name: "Римма", description: "", photo: null},
+  ],
+  title: "Same title"
+},
+  vitya_olya: {
+    persons: [
+    {name: "Витя", description: "Boy'чик Оли, он считает что он самый охуенный, красивый, невъебенный 😎", photo: "vityao.jpg"},
+    {name: "Оля", description: "Делает всех красивыми 🤩, кроме своего boy'чика 👦", photo: "olyao.jpg"},
+  ],
+  title: "👸 Красавица и Томас Шелби 💥"
+},
+  tanya_kolya: {
+    persons: [
+    {name: "Коля", description: "", photo: null},
+    {name: "Таня", description: "", photo: null},
+  ],
+  title: "Same title"
+},
+  igor_olya: {
+    persons: [
+    {name: "Игорь", description: "Опасный парень в драке 🤼‍♀️ выбил колено, но всем сказал, что на футболе. ⚽️", photo: "igor.jpg"},
+    {name: "Оля", description: "Крутая девчонка стреляет 🔫 ловко. Украла сердечко самого опасного  парня в тусе.🤓", photo: "olya_x.jpg"},
+  ],
+  title: "🔫 Второиркутские Бони & Клайд 🔫"
+},
+  stass_lisa: {
+    persons: [
+    {name: "St.Ass", description: "", photo: null},
+    {name: "ЛиZа", description: "", photo: null},
+  ],
+  title: "Same title"
+},
+  ares_family: {
+    persons: [
+    {name: "Максим", description: "Спортсмен, отец и просто красавчик", photo: null},
+    {name: "Оля", description: "", photo: null},
+  ],
+  title: "Максим и Оля"
+},
+  nastya_vitya: {
+    persons: [
+    {name: "Настя", description: "", photo: null},
+    {name: "Витя", description: "", photo: null},
+  ],
+  title: "Same title"
+},
+  baba_deda: {
+    persons: [
+    {name: "Баба", description: "", photo: null},
+    {name: "Деда", description: "", photo: null},
+  ],
+  title: "Same title"
+},
+  fucking_slaves: {
+    persons: [
+    {name: "Серёга", description: "Осветит 🔦💡 своим присутсвием самую скучную тусу 🕺", photo: "stavtcev.jpg"},
+    {name: "Лёха", description: "Может дотянуться до звезд ✨ или твоего сердечка ❤️ ", photo: "alexei.jpg"},
+    {name: "Серёга", description: "Любит фотать 18+ контент 👙 и кросы за 30к+ 🤑", photo: "traher.jpg"},
+    {name: "Тима", description: "Знает что такое бибки и клюколы, и может пояснить что тебе с этим делать 😏", photo: 'tima_slave.jpg'},
+  ],
+  title: "🏳️‍🌈 Любимые Fucking Slaves 🏳️‍🌈"
+},
+  sisters: {
+    persons: [
+    {name: "Маша", description: "", photo: null},
+    {name: "Катя", description: "", photo: null},
+  ],
+  title: "Same title"
+},
+  ira: {
+    persons: [
+    {name: "Ира", description: "Посчитает твои долги быстрее, чем ты успеешь моргнуть", photo: null}
+  ],
+  title: "Same title"
+},
+  yulya: {
+    persons: [
+    {name: "Юля", description: "Быстро домчит тебя из точки А в точку Б, даже если ее об этом не просили", photo: null}
+  ],
+  title: "Same title"
+},
+  dasha: {
+    persons: [
+    {name: "Даша", description: "", photo: null}
+  ],
+  title: "Same title"
+},
+  boba: {
+    persons: [
+    {name: "Воба", description: "", photo: null}
+  ],
+  title: "Same title"
+},
+  pashka: {
+    persons: [
+    {name: "Павлик", description: "Сделает любой ремонт в хате"}
+  ],
+  title: "Same title"
+},
+  alina: {
+    persons: [
+    {name: "Алина", description: "Подруга детства, объездила весь мир и не собирается останавливаться.", photo: null}
+  ],
+  title: "Same title"
+},
+  seva: {
+    persons: [
+    {name: "Сева aka SevOK", description: "Тот светлый миг, когда при взгляде на фото улыбаются не только глаза, но и сердце...🌈👀💝", photo: "SevOK.jpg"}
+  ],
+  title: "Увожаемый Севолод Както-тамович"
+},
+  goshan: {
+    persons: [
+    {name: "Георгий", description: "Выкурит двойное яблочко за 30сек и получит match в тиндере от любой женщины, да куда там, и от любого гетеросексуального мужчины", photo: null}
+  ],
+  title: "Same title"
+},
+  cookies: {
+    persons: [
+      {name: "Аня", description: "ышла замуж 👰🏼‍♀️ за Костю не подозревая, что он из Читы 🔪", photo: "anna_cookie.jpg"},
+      {name: "Костя", description: "Беженец🏃🏻‍♂️ с Якутии женился на Ане ради работы 👷‍♂️", photo: "cookie.jpg"},
+    ],
+    title: "🍪 Сладкие кукисы 🍪"
+  }
+};
+
+
+function guestCount(list) {
+  let counter = 2;
+  for (const guest in guest_list) {
+    if (Object.hasOwnProperty.call(guest_list, guest)) {
+      const element = guest_list[guest];
+      counter += element.persons.length;
+    }
+  }
+  return counter;
+}
+
+console.log(guestCount(guest_list));
+
+const params = new Proxy(new URLSearchParams(window.location.search), {
+  get: (searchParams, prop) => searchParams.get(prop)
+});
+
 // Add your javascript here
 // Don't forget to add it into respective layouts where this js file is needed
 $(document).ready(function() {
-  AOS.init({
-    // uncomment below for on-scroll animations to played only once
-    // once: true
-  }); // initialize animate on scroll library
+  let guest_title = $('#guest-name');
+
+  if (params.guest && params.guest in guest_list) {
+    let meta = guest_list[params.guest]
+    let guests = meta.persons;
+    guest_title.html(meta.title);
+    for (const guest in guests) {
+      if (Object.hasOwnProperty.call(guests, guest)) {
+        const element = guests[guest];
+        let card = renderGuestCard(element);
+
+        $('#guest-list').append(card);
+      }
+    }
+  } else {
+    guest_title.html('ANONYMUS');
+  }
+
+  AOS.init();
 });
+
+
+function renderGuestCard(guest) {
+  let html = '<div class="col-md-4">' +
+      '<div class="card d-block mb-3"><img class="card-img-top" src="images/groom-men-1.jpg" alt="Groom Men 1"/>'  +
+        '<div class="card-body text-center">' +
+          '<div class="h5 guest-name">{{NAME}}</div>' +
+          '<p class="mb-0 text-muted guest-description">{{description}}</p>' +
+        '</div>' +
+      '</div>' +
+    '</div>';
+
+  let template = $(html);
+  let image_path = 'images/wedding/friends/';
+
+  if (guest.photo) {
+    template.find('.card-img-top').attr('src', image_path + guest.photo); 
+  }
+
+  template.find('.guest-name').html(guest.name);
+  template.find('.guest-description').html(guest.description);
+
+  return template;
+}
+
+
+
 
 // Smooth scroll for links with hashes
 $("a.smooth-scroll").click(function(event) {
@@ -94,3 +343,12 @@ $(document).on("click", '[data-toggle="lightbox"]', function(event) {
   event.preventDefault();
   $(this).ekkoLightbox();
 });
+
+
+// Guest party
+
+
+
+
+
+
